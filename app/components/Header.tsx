d3384645e8a3846b938aff04f825a484ab36c2de
alieldeba <alieldeba375@gmail.com>
2023-05-28 00:49:00 +0300

    adding landing page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,13 +6,15 @@ import Styles from "./Header.module.css";
 
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
+import { Button } from "@mui/material";
 import Menu from "@mui/base/Menu";
 import MenuItem from "@mui/base/MenuItem";
 import IconButton from "@mui/material/IconButton";
 import ArrowRightAltOutlinedIcon from "@mui/icons-material/ArrowRightAltOutlined";
+import SendIcon from "@mui/icons-material/Send";
 
 import Image from "next/image";
-import { Button } from "@mui/material";
+import Link from "next/link";
 
 export default function Header() {
   return (
@@ -25,29 +27,31 @@ export default function Header() {
           justifyContent: "space-between",
         }}
       >
-        <Image
-          src="/logo.png"
-          width={100}
-          height={50}
-          alt="Blogify"
-          className={Styles.logo}
-        />
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            width={100}
+            height={50}
+            alt="Blogify"
+            className={Styles.logo}
+          />
+        </Link>
         <ul className={Styles.links}>
           <Button sx={{ color: "#282828" }} variant="text">
-            Home
+            <Link href="/">Home</Link>
           </Button>
           <Button sx={{ color: "#282828" }} variant="text">
-            Posts
+            <Link href="/posts">Posts</Link>
           </Button>
           <Button sx={{ color: "#282828" }} variant="text">
-            Authors
+            <Link href="/authors">Authors</Link>
           </Button>
           <Button sx={{ color: "#282828" }} variant="text">
-            Contact
+            <Link href="/contact">Contact</Link>
           </Button>
         </ul>
-        <Button sx={{ color: "#282828" }} variant="text">
-          LOGIN
+        <Button sx={{ color: "#282828" }} endIcon={<SendIcon />} variant="text">
+          <Link href="/login">LOGIN</Link>
         </Button>
       </Container>
     </Box>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,58 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
+
 import styles from "./page.module.css";
+
+import { Box, Container } from "@mui/material";
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import SendIcon from "@mui/icons-material/Send";
 
 import Header from "./components/Header";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Container
+        fixed
+        sx={{
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          gap: 10,
+        }}
+      >
+        <Box>
+          <Typography variant="h2" gutterBottom>
+            Best Place Where You Get Posts & Blogs
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            gutterBottom
+            sx={{ color: "#282828" }}
+          >
+            Try searching for our best posts and authors, and if there are
+            anything wrong just contact us.
+          </Typography>
+          <Button
+            variant="contained"
+            endIcon={<SendIcon />}
+            sx={{
+              borderRadius: "20px",
+              bgcolor: "#282828",
+              fontWeight: "300",
+              ":hover": { bgcolor: "#303030" },
+            }}
+          >
+            <Link href="/posts">See Posts</Link>
+          </Button>
+        </Box>
+        <Image src="/hero.png" width={500} height={500} alt="Blogify" />
+      </Container>
+    </>
+  );
 }
